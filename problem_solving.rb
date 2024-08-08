23438b295e0439d2d22c8e911ef773d2a9b613c6
Michael OBrien <obrien.michael.t@gmail.com>
2024-08-08 10:37:55 -0400

    Finished challenge 3

--- problem_solving.rb
+++ problem_solving.rb
@@ -25,3 +25,18 @@ test_array = [1, 2, 3]
 puts array_length(test_array)
 
 # CURRENT TIME
+def daily_greeting(current_time)
+    if current_time >= 4 && current_time <= 11
+        "Good morning!"
+    elsif current_time>= 12 && current_time <= 16
+        "Good afternoon!"
+    elsif current_time>= 17 && current_time <= 20
+        "Good evening!"
+    elsif current_time>= 21 && current_time <= 24 || current_time <= 3 && current_time > 0
+        "Good night!"
+    else
+        "Please enter a valid time!"
+    end
+end
+
+puts daily_greeting(0)
